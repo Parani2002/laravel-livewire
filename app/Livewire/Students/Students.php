bf410391278c5d5864b18dce92e153f3205d2d3a
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-27 09:14:21 +0530

    students components created

---
+++ app/Livewire/Students/Students.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Students;
+
+use Livewire\Component;
+
+class Students extends Component
+{
+    public function render()
+    {
+        return view('livewire.students.students');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
 use App\Livewire\Students\CreateStudent;
 use App\Livewire\Students\CreateStudent as StudentsCreateStudent;
+use App\Livewire\Students\EditStudent;
+use App\Livewire\Students\Students;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,5 +14,13 @@ Route::get('/', function () {
 Route::get('/counter', Counter::class);
 
 Route::get('/student/create',CreateStudent::class);
+
+Route::get('/student/edit',EditStudent::class);
+
+Route::get('/students',Students::class);
+
+
+
+
 
 // Route::post('/student',[CreateStudent::class,'save']);
