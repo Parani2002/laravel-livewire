305ba4aaf16eb8046cdf02157389e8e0cf8ea1c0
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-03-04 09:00:06 +0530

    composer update

--- resources/views/components/layouts/header.blade.php
+++
@@ -1,6 +0,0 @@
-<flux:navbar>
-    <flux:navbar.item href="#">Home</flux:navbar.item>
-    <flux:navbar.item href="#">Features</flux:navbar.item>
-    <flux:navbar.item href="#">Pricing</flux:navbar.item>
-    <flux:navbar.item href="#">About</flux:navbar.item>
-</flux:navbar>
