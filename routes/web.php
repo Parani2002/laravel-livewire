e75ad316c2c33a9fb201bb1f507904d8a37b0817
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-03-04 11:26:34 +0530

    routes update

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
+use App\Livewire\Dashboard\Dashboard;
 use App\Livewire\Students\CreateStudent;
 use App\Livewire\Students\CreateStudent as StudentsCreateStudent;
 use App\Livewire\Students\EditStudent;
 use App\Livewire\Students\Students;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',Dashboard::class);
 
 Route::get('/counter', Counter::class);
 
