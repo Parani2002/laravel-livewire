ac39c4b9187b490573f91fb19c2232ef7f5620c6
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-03-04 11:27:18 +0530

    dashboard component created

---
+++ resources/views/livewire/dashboard/dashboard.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
+        @livewire('dashboard.card',['title' => 'Students','count' => 'Studentcount'])
+        @livewire('dashboard.card',['title' => 'Teachers','count' => '122'])
+
+    </div>
+    <h1>Student Management System</h1>
+</div>
