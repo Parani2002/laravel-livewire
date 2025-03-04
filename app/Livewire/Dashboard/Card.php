dcbe0adf666ffe58e4ffd835eba9969a7eafc761
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-03-04 11:26:54 +0530

    card component created

---
+++ app/Livewire/Dashboard/Card.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire\Dashboard;
+
+use App\Models\Student;
+use Livewire\Component;
+
+class Card extends Component
+{
+    public string $title = "";
+    public int $Studentcount = 0;
+    public int $Teacherscount = 0;
+
+    public function mount($title,$count)
+    {
+        $this->Studentcount = Student::count();
+        $this->Teacherscount = 0;
+        $this->title = $title;
+      
+    }
+    
+    
+    public function render()
+    {
+        return view('livewire.dashboard.card');
+    }
+}
