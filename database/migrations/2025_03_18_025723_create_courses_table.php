0bf5052b80a68b1d68fb8643cc6f39d9732f0590
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-03-18 08:38:27 +0530

    course model mig created

---
+++ database/migrations/2025_03_18_025723_create_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('admission_fee', 13, 2);
+            $table->decimal('course_fee', 13, 2);
+            $table->integer('duration');
+            $table->string('duration_unit');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
