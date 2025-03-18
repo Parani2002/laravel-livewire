156c6bc7b6f13143d68d7b97258e14491903b573
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-03-18 08:38:11 +0530

    status updated

--- app/Livewire/Dashboard/Dashboard.php
+++ app/Livewire/Dashboard/Dashboard.php
@@ -10,7 +10,7 @@ class Dashboard extends Component
 {
     public function render()
     {
-        $pending_payments = Payment::where('status', 'failed')->get();
+        $pending_payments = Payment::where('status', 'pending')->get();
         $new_students = Student::all() ;
         return view('livewire.dashboard.dashboard', [
             'pending_payments' => $pending_payments,
